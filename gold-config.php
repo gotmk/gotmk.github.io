abf9c49ad5259c576b26f46e1def483a3b580b31
hnikoloski <45240953+hnikoloski@users.noreply.github.com>
2019-04-05 11:46:21 +0200

    first Commit

---
+++ gold-config.php
@@ -0,0 +1,12 @@
+<?php
+
+	define('GOLD_HOSTNAME', ''); // Your Localhost - default: localhost
+	define('GOLD_USERNAME', ''); // Your DB Username
+	define('GOLD_PASSWORD', ''); // Your DB Password
+	define('GOLD_DATABASE', ''); // Your DB Name
+	define('GOLD_ADMIN_USERNAME', ''); // Admin Username
+	define('GOLD_ADMIN_PASSWORD', ''); // Admin Password
+	define('GOLD_ADMIN_EMAIL', ''); // Admin Email
+	define('GOLD_SUB_FOLDER', ''); // If you uploaded files on sub-folder, please type sub-folder name
+
+?>
